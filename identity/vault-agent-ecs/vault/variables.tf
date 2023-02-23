87bb3a2796ae1dd07a2b87c62aed0fde64bed922
pastorismylord <53154863+pastorismylord@users.noreply.github.com>
2023-02-23 17:11:38 -0600

    variable migration to terraform cloud

--- identity/vault-agent-ecs/vault/variables.tf
+++ identity/vault-agent-ecs/vault/variables.tf
@@ -1,3 +1,19 @@
+variable "TFC_ORGANIZATION" {
+  type        = string
+  description = "TFC Organization for remote state of infrastructure"
+}
+
+data "terraform_remote_state" "vault-agent-ecs-infra" {
+  backend = "remote"
+
+  config = {
+    organization = var.TFC_ORGANIZATION
+    workspaces = {
+      name = "vault-agent-ecs-infra"
+    }
+  }
+}
+
 variable "name" {
   type        = string
   description = "Name for infrastructure resources"
@@ -49,10 +65,38 @@ variable "product_api_efs_access_point_arn" {
   description = "ARN for EFS Access Point of product-api"
 }
 
+variable "hcp_vault_cluster_id" {
+  type        = string
+  description = "HCP Vault Cluster ID for configuration"
+  default     = ""
+}
+
+variable "hcp_vault_admin_token" {
+  type        = string
+  description = "HCP Vault Cluster token for configuration"
+  default     = ""
+  sensitive   = true
+}
 
 locals {
-  tags = merge(var.tags, {
+    tags = merge(var.tags, {
     Service = "hashicups"
     Purpose = "learn vault-agent-ecs"
   })
+  #name                             = data.terraform_remote_state.vault-agent-ecs-infra.outputs.name
+  #ecs_cluster                      = data.terraform_remote_state.vault-agent-ecs-infra.outputs.ecs_cluster
+  region                           = var.region == "" ? data.terraform_remote_state.vault-agent-ecs-infra.outputs.region : var.region
+  #url                              = data.terraform_remote_state.vault-agent-ecs-infra.outputs.boundary_endpoint
+  product_database_username         = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_database_username
+  product_database_hostname        = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_database_hostname
+  product_database_password        = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_database_password
+  product_api_efs_access_point_arn = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_api_efs_access_point_arn
+  product_api_efs_access_point_id         = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_api_efs_access_point_id
+  product_api_endpoint       = data.terraform_remote_state.vault-agent-ecs-infra.outputs.product_api_endpoint
+  target_group_arn          = data.terraform_remote_state.vault-agent-ecs-infra.outputs.target_group_arn
+  hcp_vault_cluster_id      = var.hcp_vault_cluster_id == "" ? data.terraform_remote_state.vault-agent-ecs-infra.outputs.hcp_vault_cluster : var.hcp_vault_cluster_id
+  hcp_vault_public_endpoint  = data.terraform_remote_state.vault-agent-ecs-infra.outputs.hcp_vault_public_endpoint
+  hcp_vault_private_endpoint = data.terraform_remote_state.vault-agent-ecs-infra.outputs.hcp_vault_private_endpoint
+  hcp_vault_namespace       = data.terraform_remote_state.vault-agent-ecs-infra.outputs.hcp_vault_namespace
+  hcp_vault_admin_token   = var.hcp_vault_admin_token == "" ? data.terraform_remote_state.vault-agent-ecs-infra.outputs.hcp_vault_admin_token : var.hcp_vault_admin_token
 }
